22ba89059ecbf119b04b75e1f4d794762a207a92
mohammad mohiuddin mostafa kamal akib <mohiuddinmostafakamal@gmail.com>
2019-02-20 19:38:28 +0600

    Installed: installer .env module to separate the environment variables

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,3 @@ import $ from "jquery";
 import "materialize-css/dist/css/materialize.min.css";
 import "../css/style.css";
 import "materialize-css/dist/js/materialize.min.js";
-
-$(".heading").text("Halulu");
